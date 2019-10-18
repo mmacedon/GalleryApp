8a28dcf97c4c87cce7972752fa87198c423f8bba
Manuel <mmacedon@uci.edu>
2019-10-17 21:40:49 -0700

    Create index.py

---
+++ index.py
@@ -0,0 +1,19 @@
+import cgi
+import pymysql
+
+print("""
+        <!Doctype html>
+        <html lang='en'>
+        <head>
+            <meta charset="UTF-8"/>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+            <meta http-equiv='X-UA-Comptatible' content='ie=edge'/>
+
+            <!-- INSERT BOOTSTRAP AND PERSONAL STYLESHEET -->
+
+            <title>Gallery</title>
+        </head>
+        <body>
+            <h1>I hope this works</h1>
+        </body>
+    """)
